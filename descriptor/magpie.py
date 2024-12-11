998dcafd9c366fd3e98da717d749b11668ba8e55
lian-zhengheng <1098994933@qq.com>
2024-12-11 20:04:57 +0800

    code refactor

--- descriptor/magpie.py
+++ descriptor/magpie.py
@@ -1,11 +1,12 @@
 import pandas as pd
 
 
-def get_magpie_features(file_name="train_formula.csv", data_path="/content/") -> pd.DataFrame:
+def get_magpie_features(file_name="train_formula.csv", data_path="/content/", alloy_features=False) -> pd.DataFrame:
     """
     将含有化学式的csv文件，计算其magpie features
     :param file_name:
     :param data_path:
+    :param alloy_features: set True to add feature calculation of WenAlloys
     :return:
     """
     from matminer.featurizers.conversions import StrToComposition
@@ -16,8 +17,13 @@ def get_magpie_features(file_name="train_formula.csv", data_path="/content/") ->
     # magpie
     df_chemistry_formula = pd.read_csv(data_path + file_name)
     df_magpie = StrToComposition(target_col_id='composition_obj').featurize_dataframe(df_chemistry_formula, 'formula')
-    feature_calculators = MultipleFeaturizer([cf.Stoichiometry(), cf.ElementProperty.from_preset("magpie"),
-                                              cf.ValenceOrbital(props=['avg']), cf.IonProperty(fast=True), WenAlloys()])
+    if alloy_features:
+        feature_calculators = MultipleFeaturizer([cf.Stoichiometry(), cf.ElementProperty.from_preset("magpie"),
+                                                  cf.ValenceOrbital(props=['avg']), cf.IonProperty(fast=True),
+                                                  WenAlloys()])
+    else:
+        feature_calculators = MultipleFeaturizer([cf.Stoichiometry(), cf.ElementProperty.from_preset("magpie"),
+                                                  cf.ValenceOrbital(props=['avg']), cf.IonProperty(fast=True)])
     feature_labels = feature_calculators.feature_labels()
     df_magpie = feature_calculators.featurize_dataframe(df_magpie, col_id='composition_obj')
     return df_magpie
