c473c8e1ccbaaa64b89e2e5369254a87a08dba4e
lian-zhengheng <1098994933@qq.com>
2025-03-13 18:00:28 +0800

    bug fix

--- case_study/1_generate_alloys_by_WAE.py
+++ case_study/1_generate_alloys_by_WAE.py
@@ -13,9 +13,9 @@ if __name__ == '__main__':
     df_element = dataset[elements_col]
     # 计算合金的组分个数
     df_element["N_alloy"] = df_element.astype(bool).sum(axis=1)
+    print("原始数据")
     print(df_element.head())
     input_dim = df_element.shape[1]
-
     data = torch.Tensor(df_element.values)
 
     # 创建 WAE 训练器实例
@@ -29,26 +29,29 @@ if __name__ == '__main__':
     gen = True
     if gen:
         generated_samples_scaled = trainer.generate_samples(num_samples=10000).numpy()
+        # 反归一化得到生成样本 (成分 + 元素个数）
         generated_samples = trainer.data.scaler.inverse_transform(generated_samples_scaled)
         df_gen_source = pd.DataFrame(generated_samples_scaled, columns=df_element.columns)
         # 去掉N_alloy列
         df_gen_source.drop(columns=["N_alloy"], inplace=True)
         df_gen = pd.DataFrame(generated_samples, columns=df_element.columns)
         N_alloy = -1 * df_gen["N_alloy"].round().astype(int).copy()
-        print(N_alloy.head())
-        print(df_gen.head())
+        # print(N_alloy.head())
+        # print(df_gen.head())
+        # print(df_gen_source.head())
         # 每一行 找到前n大的元素
         for index, row in df_gen_source.iterrows():
-            n = N_alloy[index] * -1
+            n = N_alloy[index] * -1  # 找前几个最大元素
             top_n_indices = row.nlargest(n).index
             new_row = [0] * len(row)
-            new_row[-1] = n
             for col in top_n_indices:
                 new_row[df_gen_source.columns.get_loc(col)] = row[col]
             df_gen_source.loc[index] = new_row
+        df_gen_source["N_alloy"] = N_alloy
         # 归一化
         gen = trainer.data.scaler.inverse_transform(df_gen_source.values)
         df_gen = pd.DataFrame(gen, columns=df_element.columns)
+        # 归一化所有元素和为100
         df_gen = df_gen[elements_col].div(df_gen[elements_col].sum(axis=1), axis=0) * 100
         # save
-        df_gen.to_csv('../data/generated_samples_wae.csv', index=False)
+        df_gen.to_csv('generated_samples_wae.csv', index=False)
