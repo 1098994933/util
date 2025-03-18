8ea7c2c9471ed0521566daa426cd091c35bf2983
lian-zhengheng <1098994933@qq.com>
2025-03-18 13:42:13 +0800

    添加scale optimal projection

--- projection/OptimalProjection.py
+++ projection/OptimalProjection.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
 import unittest
 
 
@@ -16,7 +17,7 @@ class OptimalProjection(object):
 
         :param projections: List of PCA projection results, each element is a 2D array.
         :param y: Target values corresponding to each point in the projections.
-        :param optimal_label: 优类样本的标签，用于分类数据
+        :param optimal_label: 优类样本的标签，用于分类数据，if it is a list of scale [target_min,target_max]
         :param optimal_ratio_exponent: control the area
         """
         # 数据检测机制
@@ -29,21 +30,32 @@ class OptimalProjection(object):
             raise ValueError("y 必须是一个 NumPy 数组。")
         if len(y) != projections[0].shape[0]:
             raise ValueError("y 的长度必须与 projections 中每个投影的样本数量一致。")
-        if optimal_label is not None and optimal_label not in y:
-            raise ValueError("指定的 optimal_label 不在 y 中。")
+        if optimal_label is not None:
+            if isinstance(optimal_label, list) and len(optimal_label) == 2:
+                pass
+            elif optimal_label not in y:
+                raise ValueError("指定的 optimal_label 不在 y 中。")
 
+        self.optimal_type = None  # 优类数据的类型 属于reg 或 scale 或 cls
         self.projections = projections
         self.y = y
         self.optimal_label = optimal_label
-        if optimal_label is None:  # 判定为回归数据，则大于为优类
+        if optimal_label is None:  # 判定为回归数据，则大于为平均值优类
             self.mean_y = np.mean(y)
             self.labels = np.array(y > self.mean_y).astype(int)
+            self.optimal_type = "reg"
+        elif isinstance(optimal_label, list) and len(optimal_label) == 2:  # 某个范围为优类
+            target_min, target_max = optimal_label
+            self.labels = np.array((y >= target_min) & (y <= target_max)).astype(int)
+            self.optimal_type = "scale"
         else:
             self.labels = np.array(y == optimal_label).astype(int)
+            self.optimal_type = "cls"
         self.best_x = None
         self.best_model = None
         self.best_rectangle = None  # 最佳投影区域
         self.optimal_ratio_exponent = optimal_ratio_exponent  # 越高则优化区域面积越小
+        self.result = {}
 
     def find_best_projection(self):
         """
@@ -105,6 +117,23 @@ class OptimalProjection(object):
                 self.best_x = projection
                 self.best_rectangle = best_rect
 
+        if self.best_x is not None and self.best_rectangle is not None:
+            x_min, x_max, y_min, y_max = self.best_rectangle
+            in_rectangle = ((self.best_x[:, 0] >= x_min) & (self.best_x[:, 0] <= x_max) &
+                            (self.best_x[:, 1] >= y_min) & (self.best_x[:, 1] <= y_max)).sum()
+            num_class_1 = ((self.best_x[:, 0] >= x_min) & (self.best_x[:, 0] <= x_max) &
+                           (self.best_x[:, 1] >= y_min) & (self.best_x[:, 1] <= y_max) &
+                           (self.labels == 1)).sum()
+            area = (x_max - x_min) * (y_max - y_min)
+            ratio = num_class_1 / in_rectangle if in_rectangle > 0 else 0
+            self.result = {
+                "total_samples": in_rectangle,
+                "optimal_samples": num_class_1,
+                "optimal_ratio": ratio,
+                "optimal_area": area,
+                "score": best_product
+            }
+
         return best_index, best_product
 
     def plot_decision_boundary(self):
@@ -117,13 +146,32 @@ class OptimalProjection(object):
 
         x_min, x_max, y_min, y_max = self.best_rectangle
 
-        plt.scatter(self.best_x[:, 0], self.best_x[:, 1], c=self.labels, edgecolors='k')
+        # 使用 seaborn 绘制散点图
+        sns.scatterplot(x=self.best_x[:, 0], y=self.best_x[:, 1], hue=self.labels, palette={0: 'blue', 1: 'orange'},
+                        edgecolor='k')
         plt.title('Best Rectangle on Best Projection')
         plt.xlabel('Feature 1')
         plt.ylabel('Feature 2')
 
         # Plot the rectangle
-        plt.plot([x_min, x_max, x_max, x_min, x_min], [y_min, y_min, y_max, y_max, y_min], 'r-')
+        rectangle = plt.Rectangle((x_min, y_min), x_max - x_min, y_max - y_min, edgecolor='red', facecolor='none')
+        plt.gca().add_patch(rectangle)
+
+        # 确定优类样本标签名称
+        if self.optimal_type == "reg":
+            optimal_label_name = f"y > {self.mean_y:.2f}"
+        elif self.optimal_type == "scale":
+            target_min, target_max = self.optimal_label
+            optimal_label_name = f"{target_min:.2f} < y < {target_max:.2f}"
+        else:
+            optimal_label_name = f"y={self.optimal_label})"
+
+        # 手动添加图例
+        handles, labels = plt.gca().get_legend_handles_labels()
+        legend_labels = ['Non-Optimal Samples', f'Optimal Samples ({optimal_label_name})', 'Optimal Region']
+        legend_handles = handles + [rectangle]
+        plt.legend(legend_handles, legend_labels, loc='upper left', bbox_to_anchor=(1, 1))
+        plt.tight_layout()
         plt.show()
 
 
@@ -141,6 +189,23 @@ class TestOptimalProjection(unittest.TestCase):
         self.assertEqual(isinstance(best_product, float), True)
         self.assertTrue(0 <= best_index < len(projections))
         self.assertTrue(0 <= best_product)
+        print(op.result)
+        op.plot_decision_boundary()
+
+    def test_find_best_projection_regression_scale(self):
+        """
+        测试指定范围的最优投影查找
+        """
+        sample_num = 100
+        projections = [np.random.rand(sample_num, 2) for _ in range(100)]
+        y = 10 * np.random.rand(sample_num)
+        op = OptimalProjection(projections, y, optimal_label=[5, 10])
+        best_index, best_product = op.find_best_projection()
+        self.assertEqual(isinstance(best_index, int), True)
+        self.assertEqual(isinstance(best_product, float), True)
+        self.assertTrue(0 <= best_index < len(projections))
+        self.assertTrue(0 <= best_product)
+        print(op.result)
         op.plot_decision_boundary()
 
     def test_find_best_projection_classification(self):
@@ -157,6 +222,24 @@ class TestOptimalProjection(unittest.TestCase):
         self.assertEqual(isinstance(best_product, float), True)
         self.assertTrue(0 <= best_index < len(projections))
         self.assertTrue(0 <= best_product)
+        print(op.result)
+        op.plot_decision_boundary()
+
+    def test_find_best_projection_range(self):
+        """
+        测试 optimal_label 为范围的情况
+        """
+        sample_num = 100
+        projections = [np.random.rand(sample_num, 2) for _ in range(100)]
+        y = np.random.rand(sample_num)
+        optimal_label = [0.2, 0.8]
+        op = OptimalProjection(projections, y, optimal_label)
+        best_index, best_product = op.find_best_projection()
+        self.assertEqual(isinstance(best_index, int), True)
+        self.assertEqual(isinstance(best_product, float), True)
+        self.assertTrue(0 <= best_index < len(projections))
+        self.assertTrue(0 <= best_product)
+        print(op.result)
         op.plot_decision_boundary()
 
     def test_invalid_projections_type(self):
